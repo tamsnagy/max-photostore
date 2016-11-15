0ca5d1f0eaa6cceb62beba1551e6b91706f6a796
Tamás Nagy <ntamas13@gmail.com>
2016-11-15 22:59:59 +0100

    Context load tests fixed, by adding in memory h2 and embedded redis to test executors.

--- src/test/java/com/max/photostore/PhotostoreApplicationTests.java
+++ src/test/java/com/max/photostore/PhotostoreApplicationTests.java
@@ -1,16 +1,40 @@
 package com.max.photostore;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.embedded.RedisServer;
+import redis.embedded.RedisServerBuilder;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class PhotostoreApplicationTests {
+    private static RedisServer redisServer;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        redisServer = RedisServer.builder()
+                .port(6379)
+                .build();
+
+        redisServer.start();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        redisServer.stop();
+    }
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
---
+++ src/test/java/com/max/photostore/config/RepositoryConfigTest.java
@@ -0,0 +1,52 @@
+package com.max.photostore.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.net.URISyntaxException;
+
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.max.photostore.repository")
+@EnableAutoConfiguration
+@EntityScan(basePackages = "com.max.photostore.domain")
+@Profile("test")
+public class RepositoryConfigTest extends RepositoryConfig {
+
+
+    public RepositoryConfigTest() throws URISyntaxException {
+    }
+
+    @Override
+    protected String dbUsername() throws URISyntaxException {
+        return "username";
+    }
+
+    @Override
+    protected String dbUrl() throws URISyntaxException {
+        return "jdbc:h2:mem:test;MODE=PostgreSQL";
+    }
+
+    @Override
+    protected String dbPassword() throws URISyntaxException {
+        return "password";
+    }
+
+    @Override
+    protected int redisPort() throws URISyntaxException {
+        return 6379;
+    }
+
+    @Override
+    protected String redisHost() throws URISyntaxException {
+        return "localhost";
+    }
+
+    @Override
+    protected String redisPassword() throws URISyntaxException {
+        return null;
+    }
+}
