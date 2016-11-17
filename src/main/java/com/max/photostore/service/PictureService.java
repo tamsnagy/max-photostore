9f0d31fda397367e42fa3d78816fbbcaac9c289a
Tamás Nagy <ntamas13@gmail.com>
2016-11-17 18:03:28 +0100

    Picture upload and retrieve implemented.

---
+++ src/main/java/com/max/photostore/service/PictureService.java
@@ -0,0 +1,11 @@
+package com.max.photostore.service;
+
+import com.max.photostore.domain.Picture;
+import com.max.photostore.exception.ResourceMissingException;
+import com.max.photostore.request.UpdatePicture;
+
+public interface PictureService {
+    Long uploadPicture(byte[] bytes, String originalFilename, Long owner, Long albumId);
+    void updatePicture(Long id, UpdatePicture update);
+    Picture getPicture(Long pictureId) throws ResourceMissingException;
+}
